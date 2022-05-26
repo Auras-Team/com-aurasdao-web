b4bf08e1b7ddb695f065ae3606459730d140ad2a
Koen Van Rulo <koen.vr@vecno.io>
2022-05-26 23:43:56 +0200

    initial landing
    * setup index page
    * add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-gradient-to-r from-purple-300 to-blue-200">
+	<div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+		<div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
+			<div class="border-t border-gray-200 text-center pt-8">
+				<!-- <h1 class="text-9xl font-bold text-purple-400">404</h1> -->
+				<h1 class="text-6xl font-medium py-8">Launching Soon!</h1>
+				<p class="text-2xl pb-8 px-12 font-medium">An exchange of energy is in the making</p>
+			</div>
+		</div>
+	</div>
+</div>
