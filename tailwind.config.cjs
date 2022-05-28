a1119329bb081e8dae248c187956f3529e56f2c6
Koen Van Rulo <koen.vr@vecno.io>
2022-05-28 09:37:17 +0200

    add and setup fonts
    * The original used `Neue Helvetica`, files provided where not web safe and had missing styles / weights to compile a usable set. (also no license info)
    * Fonts Raleway/Roboto is the closest matching pair to replace the original.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		fontFamily: {
+			'sans': ['raleway', ...defaultTheme.fontFamily.sans],
+			'mono': ['roboto-mono', ...defaultTheme.fontFamily.mono],
+			'serif': ['roboto-slab', ...defaultTheme.fontFamily.serif],
+		},
 		extend: {}
 	},
 
