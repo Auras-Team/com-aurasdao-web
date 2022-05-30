d6607e0edd2daaf454cdd1a29a12a7fdabbaf397
Koen Van Rulo <koen.vr@vecno.io>
2022-05-30 10:39:48 +0200

    remove debug styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,36 +18,36 @@
 <div class="box box-size box-navi">
 	<div class="navi navi-lt">
 		{#if tokenPath}
-			<a href="/" class="text-2xl font-light opacity-75">home</a>
+			<a href="/" class="navi-link font-light opacity-75">home</a>
 		{:else}
-			<a href="/token" class="text-2xl font-light opacity-75">mint</a>
+			<a href="/token" class="navi-link font-light opacity-75">mint</a>
 		{/if}
 	</div>
 	<div class="navi navi-rt">
 		{#if infoPath}
-			<a href="/" class="text-2xl font-light opacity-75">home</a>
+			<a href="/" class="navi-link font-light opacity-75">home</a>
 		{:else}
-			<a href="/info" class="text-2xl font-light opacity-75">info</a>
+			<a href="/info" class="navi-link font-light opacity-75">info</a>
 		{/if}
 	</div>
 	<div class="navi navi-lb">
 		{#if roadmapPath}
-			<a href="/" class="text-2xl font-light opacity-75">home</a>
+			<a href="/" class="navi-link font-light opacity-75">home</a>
 		{:else}
-			<a href="/roadmap" class="text-2xl font-light opacity-75">roadmap</a>
+			<a href="/roadmap" class="navi-link font-light opacity-75">roadmap</a>
 		{/if}
 	</div>
 	<div class="navi navi-rb">
 		{#if storyPath}
-			<a href="/" class="text-2xl font-light opacity-75">home</a>
+			<a href="/" class="navi-link font-light opacity-75">home</a>
 		{:else}
-			<a href="/story" class="text-2xl font-light opacity-75">story</a>
+			<a href="/story" class="navi-link font-light opacity-75">story</a>
 		{/if}
 	</div>
 </div>
 
 <div class="box box-size box-content">
-	<div class="flex flex-col items-center justify-center py-16 pb-32 bg-slate-500">
+	<div class="flex flex-col items-center justify-center py-16 pb-32">
 		<slot />
 	</div>
 </div>
