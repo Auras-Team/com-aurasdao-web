5a432665900c4b063be06a92242c1085653db0e9
Koen Van Rulo <koen.vr@vecno.io>
2022-05-30 06:56:04 +0200

    setup page background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
+<div class="base-box">
+	<div class="base-back" />
+</div>
+<div class="focus-box">
+	<div class="focus-back" />
+</div>
+
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,6 @@
-<div class="bg-gradient-to-r from-purple-300 to-blue-200">
-	<div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
-		<div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
-			<div class="border-t border-gray-200 text-center pt-8">
-				<!-- <h1 class="text-9xl font-bold text-purple-400">404</h1> -->
-				<h1 class="text-6xl font-medium py-8">Launching Soon!</h1>
-				<p class="text-2xl pb-8 px-12 font-medium">An exchange of energy is in the making</p>
-			</div>
-		</div>
+<div class="flex flex-col items-center justify-center h-full min-h-screen">
+	<div class="flex flex-col items-center justify-center py-16 pb-32">
+		<h1 class="text-6xl font-light py-6 opacity-70">auras</h1>
+		<p class="text-2xl font-light py-2 opacity-80">An exchange of energies.</p>
 	</div>
 </div>
